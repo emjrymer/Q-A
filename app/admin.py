3b14a24c6cbd34835671cde3e61ea3070c9ca563
emjwivell <emjwivell@gmail.com>
2016-03-24 17:06:38 -0400

    update vote logic

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from app.models import UserProfile, Tag, Question, Answer
+from app.models import UserProfile, Tag, Question, Answer, Vote
 
 admin.site.register(UserProfile)
 admin.site.register(Tag)
 admin.site.register(Question)
 admin.site.register(Answer)
+admin.site.register(Vote)
--- app/models.py
+++ app/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.contrib.auth.models import User
 
 
 class UserProfile(models.Model):
     user = models.OneToOneField('auth.User', related_name='profile')
     score = models.IntegerField(default=0)
+
+    def __str__(self):
+        return str(self.user)
 
 
 # method for creating profile
@@ -30,6 +34,9 @@ class Question(models.Model):
     time_created = models.DateTimeField(auto_now_add=True)
     value = models.IntegerField(default=5)
 
+    def __str__(self):
+        return self.title
+
     class Meta:
         ordering = ['-time_created']
 
@@ -37,8 +44,15 @@ class Question(models.Model):
 @receiver(post_save, sender=Question)
 def udpate_user_score(sender, instance, **kwargs):
     instance.user.profile.score += instance.value
-    print(instance.user.profile.score)
     instance.user.profile.save()
+
+VOTE_OPTION_CHOICES = (("up", "upvote"), ("down", "downvote"))
+
+
+class Vote(models.Model):
+    answer = models.ForeignKey('app.Answer')
+    user = models.ForeignKey('UserProfile')
+    vote_choice = models.CharField(max_length=12, choices=VOTE_OPTION_CHOICES, null=True)
 
 
 class Answer(models.Model):
@@ -46,4 +60,28 @@ class Answer(models.Model):
     time_created = models.DateTimeField(auto_now_add=True)
     related_question = models.ForeignKey(Question)
     user = models.ForeignKey('auth.User')
-    score = models.IntegerField(default=0)
+    score = models.IntegerField(default=0) # don't want users to edit this
+    votes = models.ManyToManyField(UserProfile, through="app.Vote")
+
+    def __str__(self):
+        return self.body
+
+# method for updating answer score
+@receiver(post_save, sender=Vote)
+def udpate_answer_score(sender, instance, **kwargs):
+    related_user = User.objects.get(profile=instance.answer.user.profile)
+    print(related_user)
+    print(instance.vote_choice)
+    if instance.vote_choice == "up":
+        print('here')
+        related_user.profile.score += 10
+        related_user.profile.save()
+        instance.answer.score += 1
+        instance.answer.save()
+    elif instance.vote_choice == "down":
+        related_user.profile.score -= 5
+        related_user.profile.save()
+        instance.answer.score -= 1
+        instance.answer.save()
+        instance.user.score -= 1
+        instance.user.save()
