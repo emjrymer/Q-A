beb104034c322938df37c57be7073a643b2179b0
emjwivell <emjwivell@gmail.com>
2016-03-26 15:47:01 -0400

    css along with new views

--- app/views.py
+++ app/views.py
@@ -1,10 +1,10 @@
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.core.urlresolvers import reverse
-from django.views.generic import CreateView, TemplateView
+from django.views.generic import CreateView, TemplateView, DetailView
 from django.http import HttpResponseRedirect
 from rest_framework import generics
-from app.models import Question, Answer
+from app.models import Question, Answer, UserProfile
 from app.serializers import QuestionSerializer, AnswerSerializer
 
 
@@ -21,7 +21,17 @@ class IndexView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
-        context['questions'] = Question.objects.all()
+        context['questions'] = Question.objects.all()[:5]
+        return context
+
+
+class MyProfileView(DetailView):
+    model = UserProfile
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user_questions'] = Question.objects.filter(user=self.request.user)
+        context['user_answers'] = Answer.objects.filter(user=self.request.user)
         return context
 
 class QuestionListCreateAPIView(generics.ListCreateAPIView):
@@ -42,3 +52,26 @@ class AnswerListCreateAPIView(generics.ListCreateAPIView):
 class AnswerRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Answer.objects.all()
     serializer_class = AnswerSerializer
+
+
+class QuestionDetailView(DetailView):
+    model = Question
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['answers'] = Answer.objects.filter(related_question=self.kwargs.get('pk'))
+        return context
+
+class QuestionCreateView(CreateView):
+    model = Question
+    fields = ('title', 'body', 'tag')
+
+    def form_valid(self, form):
+        question_instance = form.save(commit=False)
+        question_instance.user = self.request.user
+        question_instance.save()
+        return super().form_valid(form)
+
+
+    def get_success_url(self):
+        return reverse('index_view')
--- q_n_a/urls.py
+++ q_n_a/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from django.contrib import admin
-from app.views import IndexView, SignUpView, QuestionListCreateAPIView, QuestionRetrieveUpdateDestroyAPIView, AnswerListCreateAPIView, AnswerRetrieveUpdateDestroyAPIView
+from app.views import QuestionCreateView, MyProfileView, QuestionDetailView, IndexView, SignUpView, QuestionListCreateAPIView, QuestionRetrieveUpdateDestroyAPIView, AnswerListCreateAPIView, AnswerRetrieveUpdateDestroyAPIView
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
@@ -13,4 +13,7 @@ urlpatterns = [
     url(r'^api/questions/(?P<pk>\d+)/$', QuestionRetrieveUpdateDestroyAPIView.as_view()),
     url(r'^api/answers/$', AnswerListCreateAPIView.as_view(), name='post_answer'),
     url(r'^api/answers/(?P<pk>\d+)/$', AnswerRetrieveUpdateDestroyAPIView.as_view(), name='edit_answer'),
+    url(r'^myprofile/(?P<pk>\d+)/$', MyProfileView.as_view(), name="profile"),
+    url(r'^questiondetail/(?P<pk>\d+)/$', QuestionDetailView.as_view(), name='question_detail_view'),
+    url(r'^questioncreate/$', QuestionCreateView.as_view(), name='question_create_view'),
 ]
